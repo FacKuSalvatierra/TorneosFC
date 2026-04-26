5de928d0edebd766775b650b5afc8d65e7dbe922
Facundo <devfacu1995@gmail.com>
2026-04-26 20:18:32 -0300

    build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const rajdhani = Rajdhani({
 });
 
 export const metadata: Metadata = {
-  title: "PES Torneos",
+  title: "Torneitos",
   description: "Gestión de torneos de fútbol para amigos",
 };
 
